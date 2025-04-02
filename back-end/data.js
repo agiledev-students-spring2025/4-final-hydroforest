5a53297be13b5bfcd72878de0d14bec3e40e729c
jljune9li <jljune9li@gmail.com>
2025-04-02 07:29:32 -0400

    test

---
+++ back-end/data.js
@@ -0,0 +1,10 @@
+{
+    "hydrationData": [
+      { "date": "2025-04-01", "amount": 2000, "unlockedPlant": "Oak" }
+    ],
+    "unlockedTrees": ["Oak", "Pine"],
+    "fullyGrownTrees": [],
+    "selectedTree": "Oak",
+    "growingTree": { "name": "Maple", "progress": 0 },
+    "unlockableTrees": ["Maple", "Cherry Blossom", "Baobab"]
+  }
