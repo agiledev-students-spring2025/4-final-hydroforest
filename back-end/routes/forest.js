ff141e166a7814c37a4e14261555c5b34f88271a
jljune9li <jljune9li@gmail.com>
2025-04-07 04:53:50 -0400

    save

--- back-end/routes/forest.js
+++ back-end/routes/forest.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
+const userData = require('../mock-data/data.json');
 
-const DATA_PATH = path.join(__dirname, "../mock-data/data.json");
-
-const readData = () => JSON.parse(fs.readFileSync(DATA_PATH, "utf8"));
 
 router.get("/", (req, res) => {
-  const data = readData();
-  res.json(data); // ✅ send entire data object
+  res.json({hydrationData: userData.hydrationData});
 });
 
 module.exports = router;
