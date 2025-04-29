fce6f4a5a0c2ea845236c10d69e1014781bac5ba
jljune9li <jljune9li@gmail.com>
2025-04-28 22:42:06 -0400

    fix one more locahost name

--- front-end/src/Signup.js
+++ front-end/src/Signup.js
@@ -19,7 +19,7 @@ const SignupPage = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:5005/api/auth/signup", {
+      const response = await fetch("/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
